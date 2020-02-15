fa8989ac5a6b28358445725884aaf764286b763d
Alberto Mariscal <yuniors87@gmail.com>
2020-02-15 14:08:33 -0400

    new start

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,56 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const StyledHeader = styled.header`
+  height: 6rem;
+  width: 100%;
+  padding: 0 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  color: ${props => props.theme.secondColor};
+  border-bottom: 1px solid ${props => props.theme.secondColor};
+`
+const StyledLogo = styled.div`
+  font-size: ${props => props.theme.fontSizeXL};
+`
+const StyledMenuList = styled.ul`
+  font-size: ${props => props.theme.fontSizeLarge};
+  display: flex;
+`
+const StyledMenuItem = styled.li`
+  list-style-type: none;
+  margin: 0 1rem;
+`
+const StyledLink = styled(props => <Link {...props} />)`
+  color: ${props => props.theme.secondColor};
+  text-decoration: none;
+`
+
+const Header = () => (
+  <StyledHeader>
+    <StyledLogo>alberto mariscal</StyledLogo>
+    <div>
+      <StyledMenuList>
+        <StyledMenuItem>
+          <StyledLink to="/">menu01</StyledLink>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <StyledLink to="/">menu02</StyledLink>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <StyledLink to="/">menu03</StyledLink>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <StyledLink to="/">menu04</StyledLink>
+        </StyledMenuItem>
+        <StyledMenuItem>
+          <StyledLink to="/">menu05</StyledLink>
+        </StyledMenuItem>
+      </StyledMenuList>
     </div>
-  </header>
+  </StyledHeader>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,51 +1,40 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
+import styled, { ThemeProvider } from 'styled-components'
+import Header from './header'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// #114668
+// #E44D3F
+// #FFC350
+// https://www.banzaiskate.com/
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+const theme = {
+  baseColor: '#E44D3F',
+  secondColor: '#114668',
+  fontSizeMediun: '1rem',
+  fontSizeSmall: '0.8rem',
+  fontSizeLarge: '1.5rem',
+  fontSizeXL: '3rem',
 }
+const StyledBody = styled.div`
+  background: ${props => props.theme.baseColor};
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  color: ${props => props.theme.secondColor};
+`
+const StyledContainner = styled.div`
+  height: 100vh;
+  width: 100%;
+  margin: 0 6rem;
+`
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+const Layout = ({ children }) => (
+  <ThemeProvider theme={theme}>
+    <StyledBody>
+      <Header />
+      <StyledContainner>{children}</StyledContainner>
+    </StyledBody>
+  </ThemeProvider>
+)
 
 export default Layout
